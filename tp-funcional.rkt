46c4743760be07e760fa92a2fc8d977c95be849a
juanhuaco <juanpablohuacodelcarpio@gmail.com>
2021-12-14 18:19:49 -0300

    tutto

--- tp-funcional.rkt
+++ tp-funcional.rkt
@@ -1,5 +1,42 @@
 (#%require (only racket/base random))
 
+
+;=============PARTE UNO==============
+
+
+;============funcion ambas===========
+;(define (ambos pred?)
+ ; (lambda (a b)
+;    (and (pred? a) (pred? b))))
+
+
+;============Ejercicio 1===============
+
+(define (al-menos-uno pred)
+  (lambda (a b)
+    (not (and (not (pred a)) (not (pred b))))))
+
+;============Ejercicio 2===============
+
+;(define (ni-uno-ni-otro pred)
+;  (lambda (a b)
+;    (and (not (pred a)) (not (pred b)))))
+
+;============Ejercicio 3===============
+
+(define (ni-uno-ni-otro pred)
+  (lambda (a b)
+    (not ((al-menos-uno pred) a b))))
+
+;============Ejercicio 4===============
+
+(define (ambos pred)
+  (lambda (a b)
+    (and ((al-menos-uno pred) a a) ((al-menos-uno pred) b b))))
+
+
+;=============PARTE DOS==============
+
 ;=============FUNCIONES DADAS==============
 (define conj-etiqueta "conjunto")
 
@@ -81,14 +118,52 @@
               #f)))))
 
 
+;============PARTE 3===========
 
 
+;============Ejercicio 9===========
 
+(define (pertenece obj)
+  (lambda (conj)
+    (if (member obj (cdr conj))
+        #t
+        #f)))
 
+;============Ejercicio 10===========
 
+ (define (contiene conj)
+   (lambda (obj)
+     ((pertenece obj) conj)))
 
+;============Ejercicio 11===========
 
+(define (subconj c1)
+  (lambda (c2)
+    ((para-todos (contiene c2)) c1)))
 
+;============Ejercicio 12===========
+
+(define (cardinalidad conj)
+  (if (conj-vacio? conj)
+      0
+      (+ 1 (cardinalidad ((residuo (elige conj)) conj)))))
+ 
+;============Ejercicio 13===========
+
+ (define (interseccion c1 c2)
+   (if (or (conj-vacio? c1) (conj-vacio? c2))
+       conj-vacio
+       (let ((elem (elige c1))) 
+         (if ((contiene c2) elem)
+            (conj-ampliado elem (interseccion ((residuo elem) c1) c2))
+            (interseccion ((residuo elem) c1) c2)))))
+
+;============Ejercicio 14===========
+(define (map-conj proc conj)
+  (if (conj-vacio? conj)
+      conj-vacio
+      (let ((elem (elige conj)))
+        (conj-ampliado (proc elem) (map-conj proc ((residuo elem) conj))))))
 
 
 
